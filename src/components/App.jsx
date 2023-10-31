ade8ce0fa0db8945452064904b1ddcc9d3ed825c
Regnars Klavins <regnars.klavins@gmail.com>
2023-10-31 11:37:48 +0200

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,26 @@ function App() {
     <>
     <SideBar />
     <div className="main">
-      <div className="left"></div>
+      <div className="left">
+        <div className="imageBox">
+          <div>Lets get started</div>
+          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRUiwISyFWQSsOmqPiIUCiRet5XXrtP2sBWBwHnj7HOimxnqELO" alt="" />
+        </div>
+      </div>
       <div className="right">
         <div className="registerForm">
           <h1>Register</h1>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />  
-          <button className='submit'></button>      
+          <input type="text" placeholder="Username"/>
+          <input type="text" placeholder="Email"/>
+          <input type="text" placeholder="Password"/> 
+          <div className='box'>
+          <button className='submit'>
+              Register
+              <div className="circle"></div>
+          </button> 
+          <p>Already have an account?</p>
+          <a href='google.com'>Log in</a>
+          </div> 
         </div>
       </div>
     </div>
