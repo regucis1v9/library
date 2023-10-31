292b461c34a60b865a92867b5578242af621bddc
Regnars Klavins <regnars.klavins@gmail.com>
2023-10-31 15:10:52 +0200

    Add files via upload

---
+++ src/components/Register.jsx
@@ -0,0 +1,36 @@
+import SideBar from './SideBar';
+import '../css/App.css';
+
+function Register() {
+  return (
+    <>
+    <SideBar />
+    <div className="main">
+      <div className="left">
+        <div className="imageBox">
+          <div className='message'>Lets get started</div>
+          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRUiwISyFWQSsOmqPiIUCiRet5XXrtP2sBWBwHnj7HOimxnqELO" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="registerForm">
+          <h1>Register</h1>
+          <input type="text" placeholder="Username"/>
+          <input type="text" placeholder="Email"/>
+          <input type="text" placeholder="Password"/> 
+          <div className='box'>
+          <button className='submit'>
+              Register
+              <div className="circle"></div>
+          </button> 
+          <p>Already have an account?</p>
+          <a href='login'>Log in</a>
+          </div> 
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Register;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import App from './components/App';
+import Register from './components/Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Register />
   </React.StrictMode>
 );
 
